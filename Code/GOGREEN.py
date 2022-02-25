5846c5e0d7cbc8a89e49acf8839e3e56f7cfa505
charrbit <c225h802@ku.edu>
2022-02-25 13:13:01 -0600

    GOGREEN now loads catalogs

--- Code/GOGREEN.py
+++ Code/GOGREEN.py
@@ -1,25 +1,66 @@
+from inspect import GEN_CREATED
 from astropy.table import Table
 from astropy.io import fits, ascii
-import pandas as pd 
+import pandas as pd
+
+catalogDict = {
+    'clusters': 0,
+    'photo': 1,
+    'redshift': 2,
+    'galfit': 3,
+    'matched': 4,
+}
 
 class GOGREEN:
-    def init():
-        pass
+    def __init__(self, dataPath:'Path to directory containing DR1/ and STRUCTURAL_PARA_v1.1_CATONLY/ directories') -> 'GOGREEN DATA OBJECT':
+        self.path = dataPath
+        self.cats = []
+        self.clusters_cat = self.path + 'DR1/CATS/Clusters.fits'
+        self.photo_cat = self.path + 'DR1/CATS/Photo.fits'
+        self.redshift_cat = self.path + 'DR1/CATS/Redshift_catalogue.fits'
+        self.galfit_cat = None
+        self.matched_cat = None
 
-    def generateDF(path:'Path to data file', isDAT:'If data is stored in .dat, set this flag') -> 'Pandas Dataframe':
+        self.init()
+
+    # HELPER METHODS
+    def init(self):
+        self.clusters_cat = self.generateDF(self.clusters_cat),
+        self.photo_cat = self.generateDF(self.photo_cat),
+        self.redshift_cat = self.generateDF(self.redshift_cat)
+        # Structural paramater catalogs to load are not known at startup
+        # Need to call loadStruct() to initialize
+
+        self.cats = [self.clusters_cat, self.photo_cat, self.redshift_cat, self.galfit_cat, self.matched_cat]
+
+    def generateDF(self, path: 'Path to data file', ftype: 'Data file extension type' ='fits') -> 'Pandas Dataframe':
         data = 0
-        if (isDAT):
+        if ftype == 'fits':
+            data = Table( fits.getdata( path ) ).to_pandas() 
+        elif ftype == 'dat':
             data = pd.read_csv(path, sep='\s+', engine='python', header=1)
-        else: # Otherwise, assumes .fits
-            data = Table( fits.getdata( data ) ).to_pandas() 
-            data[0] = data[0].str.rstrip().values
+
         return data
 
-    def __init__(self, path:'Path to directory containing DR1/ and STRUCTURAL_PARA_v1.1_CATONLY/ directories') -> 'GOGREEN DATA OBJECT':
-        self.clusters_cat = path + 'DR1/CATS/Clusters.fits'
-        self.photo_cat = path + 'DR1/CATS/Photo.fits'
-        self.redshift_cat = path + 'DR1/CATS/Redshift_catalogue.fits'
-        self.galfit_cat = path + 'STRUCTURAL_PARA_v1.1_CATONLY/GALFIT_ORG_CATS/'
-        self.matched_cat = path + 'STRUCTURAL_PARA_v1.1_CATONLY/STRUCTCAT_MATCHED/'
+    # USER METHODS
+    def loadStruct(self, targetCluster:'"Shortname" of structural cluster to load', cat:'galfit/matched'='matched') -> 'Pandas Dataframe':
+        galfitPath = self.path + 'STRUCTURAL_PARA_v1.1_CATONLY/GALFIT_ORG_CATS/'
+        matchedPath = self.path + 'STRUCTURAL_PARA_v1.1_CATONLY/STRUCTCAT_MATCHED/'
 
-        self.init
+        if cat == 'matched': 
+            self.cats[catalogDict[cat]] = self.generateDF(galfitPath + 'structcat_photmatch_' + targetCluster.lower() + '.dat', ftype='dat')
+        else:
+            self.cats[catalogDict[cat]] = self.generateDF(matchedPath + 'gal_' + targetCluster.lower() + '_orgcat.fits')
+
+        return self.cats[catalogDict[cat]]
+
+    def get(self, cat: 'Catalog to get ("clusters/photo/redshift/galfit/matched/all")' = 'clusters') -> 'Pandas Dataframe':
+        if (cat == 'all'):
+            return self.cats
+        return self.cats[catalogDict[cat]]
+
+    def search():
+        pass
+    
+    def match():
+        pass
